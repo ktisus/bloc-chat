7c37b110380a9e33c8d7172c0d94a268861bd016
ktisus <140302003@svuca.edu>
2019-01-14 12:11:48 -0800

    CP4_submitted

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
+import MessageList from './components/MessageList';
 
 /*<script src="https://www.gstatic.com/firebasejs/5.7.2/firebase.js"></script>
 */
   // Initialize Firebase
-  var config = {
+  const config = {
     apiKey: "AIzaSyBUyuBiByfhaM3pTOeGMhkc2-Z_l0lYhFU",
     authDomain: "bloc-chat-f0c4c.firebaseapp.com",
     databaseURL: "https://bloc-chat-f0c4c.firebaseio.com",
@@ -19,13 +20,29 @@ import RoomList from './components/RoomList';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      activeRoom: ''
+    }
+    this.setRoom = this.setRoom.bind(this);
+  }
+
+  setRoom(room) {
+    this.setState({ activeRoom: room });
+    console.log(this.state.activeRoom)
+  }
+
   render() {
     return (
         <div className="App">
-        <header>
-          
-          <RoomList firebase={ firebase } />
-        </header>
+          <RoomList
+          firebase={ firebase }
+          setRoom = {(room) => this.setRoom(room) } />
+          <MessageList
+          firebase={ firebase }
+          activeRoom = { this.state.activeRoom }
+          username = '' />
       </div>
     );
   }
